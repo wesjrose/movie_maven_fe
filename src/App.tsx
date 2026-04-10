b18648ee61f61fff8201fad6d4920d4a5eaec3a3
Wes Rose <wesjrose@gmail.com>
2026-04-10 16:45:53 -0400

    chore: clean up App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import { useState } from "react";
 import "./App.css";
 import AppLayout from "./components/AppLayout/AppLayout";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <AppLayout></AppLayout>

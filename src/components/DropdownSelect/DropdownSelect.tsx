c7343bfc1af60e64aac5a8bfae3d778a9450eefe
Wes Rose <wesjrose@gmail.com>
2026-04-17 15:49:01 -0400

    feat: add ellipses and tool tip for multi-select tooltip

--- src/components/DropdownSelect/DropdownSelect.tsx
+++ src/components/DropdownSelect/DropdownSelect.tsx
@@ -9,6 +9,8 @@ import {
 import { ChevronUpDownIcon } from "@heroicons/react/16/solid";
 import { CheckIcon } from "@heroicons/react/20/solid";
 
+const labelLength: number = 10;
+
 const items = [
   {
     id: 1,
@@ -34,15 +36,26 @@ const items = [
 
 export default function Example() {
   const [selected, setSelected] = useState<{ id: number; name: string }[]>([]);
+  const displayText: string =
+    selected.length > 0 ? selected.map((s) => s.name).join(", ") : "All Genres";
+  const truncDisplayText: string =
+    displayText.length > labelLength
+      ? displayText.slice(0, labelLength) + "..."
+      : displayText;
 
   return (
     <Listbox value={selected} onChange={setSelected} multiple>
       <div className="relative w-fit pl-5 pr-5 mt-0">
         <ListboxButton className="grid w-auto cursor-default grid-cols-1 rounded-md bg-white py-1.5 pr-2 pl-3 text-left text-black outline-1 -outline-offset-1 outline-white/10 focus-visible:outline-2 focus-visible:-outline-offset-2 focus-visible:outline-indigo-500 sm:text-sm/6">
           <span className="col-start-1 row-start-1 flex items-center gap-3 pr-6">
-            {selected.length > 0
-              ? selected.map((s) => s.name).join(", ")
-              : "All Genres"}
+            <span className="relative group">
+              {displayText.length > labelLength && (
+                <div className="absolute bottom-full mb-2  hidden group-hover:block bg-gray-800 text-white text-xs rounded px-2 py-1 whitespace-nowrap">
+                  {displayText}
+                </div>
+              )}
+              {truncDisplayText}
+            </span>
           </span>
           <ChevronUpDownIcon
             aria-hidden="true"

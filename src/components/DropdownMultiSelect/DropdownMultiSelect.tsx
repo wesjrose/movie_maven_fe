07c0d28f8226556d1b4c762d3d516cb74079697f
Wes Rose <wesjrose@gmail.com>
2026-04-17 16:47:42 -0400

    feat: change some of the styling

--- src/components/DropdownMultiSelect/DropdownMultiSelect.tsx
+++ src/components/DropdownMultiSelect/DropdownMultiSelect.tsx
@@ -80,15 +80,15 @@ export default function DropdownMultiSelect(props: DropdownMultiSelectProps) {
             <ListboxOption
               key={person.id}
               value={person}
-              className="group relative cursor-default py-2 pr-12 pl-3 text-black select-none data-focus:bg-indigo-500 data-focus:outline-hidden"
+              className="group relative cursor-default py-2 pr-12 pl-3 text-black select-none data-focus:bg-[var(--blue-selected)] data-focus:outline-hidden"
             >
               <div className="flex items-center">
-                <span className="ml-3 block truncate font-normal group-data-selected:font-semibold">
+                <span className="ml-3 block truncate font-normal group-data-selected:font-normal">
                   {person.name}
                 </span>
               </div>
 
-              <span className="absolute inset-y-0 right-0 flex items-center pr-4 text-indigo-400 group-not-data-selected:hidden group-data-focus:text-white">
+              <span className="absolute inset-y-0 right-0 flex items-center pr-4 text-[var(--blue-selected)] group-not-data-selected:hidden group-data-focus:text-white">
                 <CheckIcon aria-hidden="true" className="size-5" />
               </span>
             </ListboxOption>
